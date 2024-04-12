ba88822b476aeab019d2af691d3e933fa611e461
ButterNutCrinklefrys <166453109+ButterNutCrinklefrys@users.noreply.github.com>
2024-04-12 15:13:29 -0700

    DischargeUpdated.py
    Revamped the system, options are available. Simply need to add patient and edits to said patient.

--- Discharge.py
+++ Discharge.py
@@ -1,30 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 class Person:
 
-    def __init__(self, first_name, last_name, department):
+    def __init__(self, first_name, last_name, total_bill):
         
         self.first_name = first_name
         self.last_name = last_name
-        self.department = department
+        self.totatl_bill = total_bill
         
-        
-
-
-# In[3]:
-
-
 class Patient_Discharge(Person):
     
-    def __init__(self, first_name, last_name, department, Discharge):
-        super().__init__(self, first_name, last_name, department)
-        self.Discharged = Discharged
+    def __init__(self, first_name, last_name, total_bill):
+        super().__init__(self, first_name, last_name)
+        self.total_bill = total_bill
 
-    def options():
+    def options(self):
 
         flag = True
         
@@ -32,29 +20,31 @@ class Patient_Discharge(Person):
             
             print(f"Please enter an option one of the following options\n")
 
-            print(f"The options are:\n 'Exit' To Leave the System. \n 'Discharge' to Discharge a Patient \n 'Return' to Return to Main Menu.")
+            print(f"The options are:\n 'Exit' To Leave the System. \n 'Discharge' to Discharge a Patient \n")
             
             user_input = input().capitalize()
             
             if user_input == 'Exit':
-                print("You have entered {self.user_input} and have left the system")
+                print(f"You have entered \"{user_input}\" and have left the system")
                 flag = False
             
-            if user_input == 'Discharge':
+            elif user_input == 'Discharge':
                 
-                
-                flag = False
+                altered_info = (, )  
+                with open(f"{Patient_1.first_name}_disachareged.txt", 'w'):
+                    
 
-            if user_input == 'Return':
+                
+                
                 flag = False
 
             else: 
                 print("Hmm, seems like an error occured, please try again.\n")
+       
         while flag == False:
             return
 
-# Patient_1 = Patient_Discharge("John", "Doe", "Pysch Ward", "False")
+# Patient_1 = Patient_Discharge("John", "Doe", "$20,000")
 
 
-Patient_Discharge.options()
-
+Patient_1.options()
